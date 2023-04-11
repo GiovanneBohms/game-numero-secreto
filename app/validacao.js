d01d04c7d56d4f8e0419105045ecfb79645d49b5
giovannebohms <giovannebohms@gmail.com>
2023-04-11 17:48:47 -0300

    aviso de valor maior ou menor

--- app/validacao.js
+++ app/validacao.js
@@ -3,18 +3,25 @@ function verificaSeOChutePossuiValorValido(chute){
 
     if (chuteForInvalido(numero)){
         elementoChute.innerHTML +=`<div>valor inválido</div>`
+        return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML +=`
         <div>Valor fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
     }
 
     if (numero === numeroSecreto){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3> O número secreto era ${numeroSecreto}</h3>
-        `
+            `   
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += ` <div> O Número secreto é menor <i class="fa-solid fa-down-long"></i></div>` 
+    } else  {
+        elementoChute.innerHTML += ` <div> O Número secreto é maior <i class="fa-solid fa-up-long"></i></div>` 
+
     }
 }
 
